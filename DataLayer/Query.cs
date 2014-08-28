021fde29b552a6fb9358d7cdd188e151fa04c640
Enrico Della Monica <enricodellamonica@gmail.com>
2014-08-28 13:36:56 +0200

    first

---
+++ DataLayer/Query.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainClasses;
+
+namespace DataLayer {
+    public class Query
+    {
+        readonly PasswordEditConverterModel _db = Db.ConnectionString;
+          
+
+        public bool CheckUserAuthetication(string box1, string box2) {
+
+             
+            bool doesUserExist = _db.Users.Any(u => u.Name == box1 && u.Password == box2);
+
+            return doesUserExist;
+
+            }
+
+        public void UpdatePassword(string name, string oldPass, string newPass)
+        {
+           var pass= _db.Users.FirstOrDefault(c => c.Name == name && c.Password == oldPass);
+            pass.Password = newPass;
+            _db.SaveChanges();
+
+        }
+        
+
+
+
+            }
+
+
+       
+        }
+
+    
---
+++ PasswordEdit/Login.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using DataLayer;
+
+namespace PasswordEdit {
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window {
+        private Query q;
+        public Login() {
+            InitializeComponent();
+            q = new Query();
+            new MockUser("fabio", "scopel");
+            }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+        if(q.CheckUserAuthetication(TBUser.Text, TBPassword.Text))
+        {
+
+
+            TbEdit.Visibility=System.Windows.Visibility.Visible;
+            BtEdit.Visibility = System.Windows.Visibility.Visible;
+        }
+        else {
+            MessageBox.Show("username or password is invalid");
+
+            }
+
+            }
+
+        private void BtEdit_Click(object sender, RoutedEventArgs e) {
+                q.UpdatePassword(TBUser.Text, TBPassword.Text,TbEdit.Text);
+            }
+        }
+    }
